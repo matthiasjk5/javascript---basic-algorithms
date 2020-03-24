dc92e975d09f0668601280e6c3ee777de2bac0ed
Andy Ko <56237597+matthiasjk5@users.noreply.github.com>
2020-03-23 23:38:25 -0400

    confirm the ending

--- Return Largest Numbers in Arrays-1.js
+++ Return Largest Numbers in Arrays-1.js
@@ -4,7 +4,7 @@ function largestOfFour(arr) {
   arr.forEach(t => {
     largest = t[0];
     for (let i = 1; i < t.length; i++) {
-      if (t[i] > t[0]) {
+      if (t[i] > largest) {
         largest = t[i];
       }
     }
@@ -16,8 +16,8 @@ function largestOfFour(arr) {
 
 console.log(
   largestOfFour([
-    [4, 5, 1, 3],
     [13, 27, 18, 26],
+    [4, 5, 1, 3],
     [32, 35, 37, 39],
     [1000, 1001, 857, 1]
   ])
