68326a20a91e1a56156ef6d554a9f0de6b5c47ae
Andy Ko <56237597+matthiasjk5@users.noreply.github.com>
2020-04-04 16:06:39 -0400

    Create where do i belong.js

---
+++ where do i belong.js
@@ -0,0 +1,11 @@
+function getIndexToIns(arr, num) {
+  arr.sort((a, b) => a - b);
+  console.log(arr);
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] >= num) return i;
+  }
+
+  return arr.length;
+}
+
+console.log(getIndexToIns([2, 5, 10], 15));
